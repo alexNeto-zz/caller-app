fe2c57412354663400be74f9853bde6beca6b288
Alexandre Neto <alex.neto3@hotmail.com>
2016-12-08 05:24:38 -0200

    Update sketch_controle_2.1.0.ino

--- Arduino/sketch_controle_2.1.0.ino
+++ Arduino/sketch_controle_2.1.0.ino
@@ -4,82 +4,51 @@
 ********/
 
 
-#include <SPI.h>
-#include <MFRC522.h> 
-#include <SoftwareSerial.h>
+#include <SPI.h> // Possibilita a utilização de uma interface serial.
+#include <MFRC522.h> //Facilita a utilização do módulo RFID.
+#include <SoftwareSerial.h> //Possibilita a utilização de qualquer 
+                            //porta digital de forma serial.
 
 #define SS_PIN 10
 #define RST_PIN 9
-
+// define as portas que serao usadas como serial
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 SoftwareSerial BTserial(2, 3);
- 
-byte ATmodePin=4;
-char c = ' ';
+
 void setup(){
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
+    // Define a porta como saida
+    pinMode(7, OUTPUT); // LED Vermelho
+    pinMode(8, OUTPUT); // LED Verde
     /************************************************************
-    *************** SETUP OF THE BLUETOOTH MODULE ***************
-    *************************************************************/
-    // set up the pin used to turn on AT mode
-    pinMode(ATmodePin, OUTPUT); 
-    digitalWrite(ATmodePin, LOW); 
-    // HC-05 default serial speed for communication mode is 9600
+    ************* INICIALIZAÇAO DO MODULO BLUETOOTH *************
+    *************************************************************/ 
+    // Velocidade de comunicaçao padrao do HC-05 e 9600
     BTserial.begin(9600);
    /************************************************************
-    **************** SETUP OF THE SERIAL PORT *******************
+    ************* INICIALIZAÇAO DA PORTA SERIAL ****************
     *************************************************************/
-    // Start the serial monitor
+    // Inicia monitor serial
     Serial.begin(9600);
-    Serial.println("DBMP initialized"); 
+    Serial.println("DBMP initialized"); // DBMP e o nome enquanto projeto
     /************************************************************
-    ***************** SETUP OF THE RFID MODULE ******************
+    ************** INICIALIZAÇAO DO MODULO RFID *****************
     *************************************************************/
-    SPI.begin(); // init SPI bus
-    mfrc522.PCD_Init(); // init MFRC522
-    /************************************************************
-    **************** PRINT THE INIT MESSAGE *********************
-    *************************************************************/
-    //Serial.println("BTserial started at 9600"); /**** message init ****/
-    //Serial.println("Type # to enter AT mode");  /*** of the program ***/
-    //Serial.println("Bring the card closer");    /*** will be changed **/
+    SPI.begin(); // inicia SPI bus
+    mfrc522.PCD_Init(); // inicia MFRC522
 }
-/***********************************************************************
-*************** SUBROUTINE TO RECEIVE AND SEND DATA ********************
-************************** BY BLUETOOTH ********************************/
-void atMode(char c){
-// Keep reading from Arduino Serial Monitor and send to HC-05
-  /********************************************************************
-  ********************* ENTER AND EXIT AT MODE ************************
-  *********************************************************************/ 
-  
-        if (c=='#'){  // enter AT mode
-            digitalWrite(ATmodePin, HIGH);
-            Serial.print("Entered AT mode. Type $ to exit");
-        }
- 
-        else if (c=='$'){ // exit AT mode by reseting the HC-05
-            digitalWrite(ATmodePin, LOW);
-            BTserial.print("AT+RESET\n\r");
-            Serial.print("AT+RESET\n\r"); 
-        }        
- 
-        else{
-              // Copy the serial data back to to the serial monitor. 
-              // This makes it easy to follow the commands and replies
-              Serial.write(c);
-              BTserial.write(c);  
-        }
- }
+/*****************************************************************
+*********** SUBROTINA PARA RECEBER E ENVIAR DADOS ****************
+********************** PELO BLUETOOTH ****************************/
 void bluetooth(String idCard){        
-   // Copy the serial data back to to the serial monitor. 
-   // This makes it easy to follow the commands and replies
-   BTserial.println(idCard);
-  
+   BTserial.println(idCard); // envia para aparelho conectado
 }
+/*****************************************************************
+*********** SUBROTINA PARA RECEBER UID DO CARTAO *****************
+************************* PELO RFID ******************************/
+
 String rfid(){  
-   if (! mfrc522.PICC_IsNewCardPresent()){
+  // Verifica se ha cartao
+  if (! mfrc522.PICC_IsNewCardPresent()){
     return "";
   }
   // Select one of the cards
@@ -95,46 +64,35 @@ String rfid(){
      conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  //Serial.println();
-  //Serial.print("Mensagem : "); monitor serial do arduino ide
+  // formata o MAC address como caixa alta
   conteudo.toUpperCase();
+  // Salva apenas o MAC address na string
   String idCard = conteudo.substring(1);
   return idCard;
   
 }
 void loop(){
   digitalWrite(7, HIGH);
-  // Keep reading from HC-05 and send to Arduino Serial Monitor
-  if (BTserial.available()){  
-    c = BTserial.read();
-      Serial.write(c);
-      BTserial.write(c); 
-  }
   // read CARD
-  String idCard = rfid();
-   if(idCard != ""){
+  String idCard = rfid(); // chama a funcao rfid() 
+                          // e salva na string o valor
+                          // retornado
+   if(idCard != ""){      // se houver informaço para ser enviada 
+     // digitalWrite() sao para a sequencia de sinalizacao
      digitalWrite(7, LOW);
      digitalWrite(8, HIGH);
-     delay(500);
-     bluetooth(idCard);
+     bluetooth(idCard);  // chama a funço bluetooth e envia o MAC
+     delay(750);
      digitalWrite(8, LOW);
-     delay(250);
      digitalWrite(7, HIGH);
-     delay(250);
+     delay(500);
      digitalWrite(7, LOW);
      delay(250);
      digitalWrite(7, HIGH);
-     delay(750);
+     delay(500);
+     
    }
-   idCard = "";
-   // Arduino to HC-05
-   if(Serial.available()){
-     c = Serial.read();
-     if(c != ' ')
-       atMode(c);
-   }
- 
+   // zera a variavel
+   idCard = ""; 
 }  
 /**********************************************************************************/
-    
-
